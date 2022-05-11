b9a23e4dfdd7c0d99c8fda6480206b734a352b50
Maxime Moreillon <m.moreillon@gmail.com>
2022-05-11 17:00:26 +0300

    pointmass

---
+++ PointMass.js
@@ -0,0 +1,41 @@
+
+class PointMass {
+
+    constructor(options = {}){
+  
+      const {
+        mass = 1,
+      } = options
+  
+      this.position = { x: 0, y: 0 }
+      this.velocity = { x: 0, y: 0 }
+      this.acceleration = { x: 0, y: 0 }
+  
+      this.mass = mass
+    }
+
+    apply_force(force){
+        const components = ['x', 'y']
+  
+        components.forEach( component => {
+            this.acceleration[component] = force[component] / this.mass
+      })
+    }
+  
+    step(delta_time) {
+
+
+        const delta_time_s = delta_time / 1000
+        const components = ['x', 'y']
+
+        components.forEach( component => {
+            this.velocity[component] += this.acceleration[component] * delta_time_s
+            this.position[component] += this.velocity[component] * delta_time_s
+        })
+      
+    }
+  
+  
+  }
+
+  export { PointMass }
--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-function step(last_time){
+import {PointMass} from './PointMass.js'
+
+
+const pointMass = new PointMass()
 
 
 
-  window.requestAnimationFrame(() => {
+function step(last_time = 0){
+
+  window.requestAnimationFrame( () => {
     const current_time = new Date()
     const delta_time = current_time - last_time
 
-    console.log(delta_time);
+    pointMass.step(delta_time)
+    pointMass.apply_force({x: 1})
+
+    console.log(pointMass.position.x)
+
+
     step(current_time)
-  });
+  })
 
 
 }
 
+
 step()
+
+
+
